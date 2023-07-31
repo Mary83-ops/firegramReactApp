faba256b47fe12f518647676282cb4b0761f1521
dansantos2 <danielsana60@gmail.com>
2023-07-31 15:44:08 +0300

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase/app'
+import 'firebase/storage'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAFsdqNkyPA-0B1e5Wqa21SNt-aDPzIP68",
+    authDomain: "dan-firegram-bd286.firebaseapp.com",
+    projectId: "dan-firegram-bd286",
+    storageBucket: "dan-firegram-bd286.appspot.com",
+    messagingSenderId: "164507447616",
+    appId: "1:164507447616:web:40631d7e07770697880aa5"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export {projectStorage, projectFirestore};
